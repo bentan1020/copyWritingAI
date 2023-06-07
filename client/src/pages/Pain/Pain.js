c1f4f82f3ec89f1abf9830dfb0436214b8958701
ben <73725152+bentan1020@users.noreply.github.com>
2023-06-07 17:59:12 -0400

    pain page finished

--- client/src/pages/Pain/Pain.js
+++ client/src/pages/Pain/Pain.js
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
+import { Grid } from "@mui/material";
+
+import Headers from "../../components/Header/Header";
+import Response from "../../components/Response/Response";
 
 const Pain = () => {
-  return (
-    <div>Pain</div>
-  )
-}
+  const [response, setResponse] = useState("");
+  const [painData, setPainData] = useState({
+    psychological: "",
+    health: "",
+    economic: "",
+    relationship: "",
+  });
 
-export default Pain
+  const changeHandler = (id, value) => {
+    setPainData((prevPainData) => ({
+      ...prevPainData,
+      [id]: value,
+    }));
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:8080/pain", painData)
+      .then((res) => {
+        console.log(res.data)
+        setResponse(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <>
+      <Headers></Headers>
+      <Grid container spacing={0}>
+        <Grid item xs={6}>
+          <form onSubmit={submitHandler}>
+            {Object.keys(painData).map((info) => (
+              <div key={info}>
+                <p style={{ textTransform: "capitalize" }}>{info}</p>
+                <input
+                  id={info}
+                  value={painData[info]}
+                  onChange={(e) => changeHandler(e.target.id, e.target.value)}
+                />
+              </div>
+            ))}
+            <button type="submit">Next</button>
+          </form>
+        </Grid>
+        <Grid item xs={6}>
+          <Response response={response}></Response>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default Pain;
--- server/index.js
+++ server/index.js
@@ -32,6 +32,24 @@ app.post("/general", async (req, res) => {
   res.send(response.data.choices[0].text);
 });
 
+app.post("/pain", async (req, res) => {
+  const input = req.body;
+
+  let feedData = "my client is dealing with these pain: \n";
+  Object.entries(input).forEach(([key, value]) => {
+    feedData += `${key}: ${value}, `;
+  });
+
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `repeat my data back to me: ${feedData}.`,
+    max_tokens: 150,
+    temperature: 0,
+  });
+
+  res.send(response.data.choices[0].text);
+});
+
 const port = 8080;
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
